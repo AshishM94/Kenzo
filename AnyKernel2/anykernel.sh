27cec94aa840fdedc99adb31ac04d447302ca5d3
AshishM94 <im.ashish994@gmail.com>
2016-12-02 10:11:56 +0530

    kenzo: add AnyKernel2 source cleaned up and added support for kenzo credits to osm0sis <osm0sis@outlook.com> for AnyKernel2

---
+++ AnyKernel2/anykernel.sh
@@ -0,0 +1,42 @@
+# AnyKernel2 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# EDIFY properties
+kernel.string=Zetsubou by Ashish94 @ xda-developers
+do.devicecheck=1
+do.initd=1
+do.modules=0
+do.cleanup=1
+device.name1=kenzo
+device.name2=Redmi Note 3
+device.name3=
+device.name4=
+device.name5=
+
+# shell variables
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=0;
+
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. /tmp/anykernel/tools/ak2-core.sh;
+
+
+## AnyKernel permissions
+# set permissions for included ramdisk files
+chmod -R 755 $ramdisk
+
+
+## AnyKernel install
+dump_boot;
+
+# begin ramdisk changes
+
+# end ramdisk changes
+
+write_boot;
+
+## end install
+
