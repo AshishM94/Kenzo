96125e35867cc6c246d1a1d9dd231bc8cee796bc
AshishM94 <im.ashish994@gmail.com>
2017-01-01 09:22:08 +0530

    build script: switch to gcc6

--- build.sh
+++ build.sh
@@ -26,7 +26,7 @@ yellow='\033[0;33m'
 red='\033[0;31m'
 nocol='\033[0m'
 # Modify the following variable if you want to build
-export CROSS_COMPILE="/home/saiko94/kernel/toolchain/bin/aarch64-unknown-linux-gnueabi-"
+export CROSS_COMPILE="/home/saiko94/x-tools/aarch64-unknown-linux-gnueabi/bin/aarch64-unknown-linux-gnueabi-"
 export ARCH=arm64
 export SUBARCH=arm64
 export KBUILD_BUILD_USER="ashish.m"
