6c760bec1353aaa0d781b443caca26d5b510a000
AshishM94 <im.ashish994@gmail.com>
2017-01-02 10:55:35 +0530

    input: xpad: fix compile error

--- drivers/input/joystick/xpad.c
+++ drivers/input/joystick/xpad.c
@@ -1107,9 +1107,6 @@ static int xpad_init_input(struct usb_xpad *xpad)
 	struct input_dev *input_dev;
 	int i, error;
 
-	if (intf->cur_altsetting->desc.bNumEndpoints != 2)
-		return -ENODEV;
-
 	input_dev = input_allocate_device();
 	if (!input_dev)
 		return -ENOMEM;
@@ -1203,6 +1200,9 @@ static int xpad_probe(struct usb_interface *intf, const struct usb_device_id *id
 	struct usb_endpoint_descriptor *ep_irq_in;
 	int ep_irq_in_idx;
 	int i, error;
+
+	if (intf->cur_altsetting->desc.bNumEndpoints != 2)
+		return -ENODEV;
 
 	for (i = 0; xpad_device[i].idVendor; i++) {
 		if ((le16_to_cpu(udev->descriptor.idVendor) == xpad_device[i].idVendor) &&
