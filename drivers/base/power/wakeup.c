4512d3d7d7f6fc4bc3f51491f0bbe85fc3e2e53f
AshishM94 <im.ashish994@gmail.com>
2017-04-04 10:45:19 +0530

    drivers: wakeup: add option to block wlan, alarmtimer & bluetooth_timer wakelocks

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -29,6 +29,12 @@ static bool enable_timerfd_ws = true;
 module_param(enable_timerfd_ws, bool, 0644);
 static bool enable_netlink_ws = true;
 module_param(enable_netlink_ws, bool, 0644);
+static bool enable_wlan_ws = true;
+module_param(enable_wlan_ws, bool, 0644);
+static bool enable_alarmtimer_ws = true;
+module_param(enable_alarmtimer_ws, bool, 0644);
+static bool enable_bluetooth_timer_ws = true;
+module_param(enable_bluetooth_timer_ws, bool, 0644);
 
 /*
  * If set, the suspend/hibernate code will abort transitions to a sleep state
@@ -465,7 +471,13 @@ static bool wakeup_source_blocker(struct wakeup_source *ws)
 			(!enable_timerfd_ws &&
 				!strncmp(ws->name, "[timerfd]", wslen)) ||
 			(!enable_netlink_ws &&
-				!strncmp(ws->name, "NETLINK", wslen))) {
+				!strncmp(ws->name, "NETLINK", wslen)) ||
+			(!enable_timerfd_ws &&
+				!strncmp(ws->name, "wlan", wslen)) ||
+			(!enable_timerfd_ws &&
+				!strncmp(ws->name, "alarmtimer", wslen)) ||
+			(!enable_timerfd_ws &&
+				!strncmp(ws->name, "bluetooth_timer", wslen))) {
 			if (ws->active) {
 				wakeup_source_deactivate(ws);
 				pr_info("forcefully deactivate wakeup source: %s\n",
